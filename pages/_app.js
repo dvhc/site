6c536dc2f62d1cd9eaa19e2495ba3d6f5b1b21ca
Neel Redkar <neel.redkar@gmail.com>
2020-08-13 21:04:44 -0700

    add hackclub logo

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,28 @@
 import React from "react"
 import { ThemeProvider } from "theme-ui"
+import "../components/style.css"
 import theme from "../components/theme"
 import Footer from "../components/Footer"
-import { Flex } from "rebass"
+import { Flex, Image } from "rebass"
 import Nav from "../components/Nav"
 import Head from "next/head"
+import Banner from "@hackclub/banner"
 
 export default ({ props, Component }) =>
     <ThemeProvider theme={theme}>
         <Head>
             <title>DVHackClub</title>
         </Head>
+        <Banner year={2020} style={{ width: '256px' }} />
+        <Flex className="rotate" sx={{
+            display: ["none", "block", "block"],
+            position: "absolute",
+            width: "256px",
+            top: "-10px",
+            left: "10px",
+        }} as="a" href="https://hackclub.com">
+            <Image sx={{ height: "100%" }} src="https://assets.hackclub.com/flag-orpheus-top.svg" />
+        </Flex>
         <Flex height="100%" flexDirection="column" minHeight="100vh">
             <Nav />
             <Component {...props} />
